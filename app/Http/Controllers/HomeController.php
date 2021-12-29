2d2dddaeed8abcd2097d5d68d8d0d94baa3f1ce2
Ashraf Uddin <ashrafuddin765@gmail.com>
2021-12-29 08:39:17 +0600

    'test'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller {
     public function home() {
 
         $latest_properties = Property::latest()->get()->take( 4 );
-        $locations         = Location::select( ['id', 'city '] )->get();
+        $locations         = Location::select( ['id', 'city'] )->get();
 
         return view( 'welcome', [
             'latest_properties' => $latest_properties,
