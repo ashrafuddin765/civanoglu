a8695007c1c043b5b10ab45478c53ee3880c0e02
Ashraf Uddin <ashrafuddin765@gmail.com>
2021-12-29 08:35:23 +0600

    Tutorial 14 completed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use App\Models\Property;
 
 class HomeController extends Controller {
     //
     public function home() {
 
-        $latest_properties = Property::latest()->get()->take(4);
+        $latest_properties = Property::latest()->get()->take( 4 );
+        $locations         = Location::select( ['id', 'city '] )->get();
 
         return view( 'welcome', [
             'latest_properties' => $latest_properties,
-        ]);
+            'locations'         => $locations,
+        ] );
     }
 }
